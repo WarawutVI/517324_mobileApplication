2b4e45dc917df3224bc836b51fa4cf59a6e02c4d
Warawut Vichaiya <148454248+WarawutVI@users.noreply.github.com>
2026-02-07 14:34:22 +0700

    button add remove reset exercise

--- lib/components/like_card.dart
+++ lib/components/like_card.dart
@@ -6,30 +6,45 @@ class LikeCard extends StatefulWidget {
   State<LikeCard> createState() => _LikeCardState();
 }
 
-
-
 class _LikeCardState extends State<LikeCard> {
   bool isLiked = false;
+  int count = 0;
   @override
   Widget build(BuildContext context) {
-
-
-
     return Container(
-      color: Colors.lightGreenAccent,
+      height: 100,
       child: Column(
         children: [
-          Text(
-            isLiked ? "Liked" : " DisLike"
-            ),
-          IconButton(
-            onPressed: () {
-              setState(() {
-                isLiked = !isLiked;
-              });
-            }, 
-            icon: Icon(
-              isLiked ? Icons.thumb_up : Icons.thumb_down)),  
+          Text(count.toString(),
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    count++;
+                  });
+                },
+                icon: Icon(Icons.add),
+                color: Colors.redAccent,
+                
+              ), IconButton(
+                onPressed: () {
+                  setState(() {
+                    count--;
+                  });
+                },
+                icon: Icon(Icons.remove),
+                color: Colors.redAccent,
+              ),TextButton(
+                onPressed: (){
+                  setState(() {
+                    count=0;
+                  });
+                }, child: Text("Reset",style: TextStyle(color: Colors.redAccent),))
+            ],
+          ),
         ],
       ),
     );
