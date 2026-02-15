83a7881e50df055263766ba84d1015806509f75c
Warawut Vichaiya <148454248+WarawutVI@users.noreply.github.com>
2026-02-15 10:20:15 +0700

    textcontroller  and setstate
    setstate use with text or value U wnat it change and display realtime

--- lib/pages/workshop_page.dart
+++ lib/pages/workshop_page.dart
@@ -12,61 +12,76 @@ class WorkshopPage extends StatefulWidget {
 class _WorkshopPageState extends State<WorkshopPage> {
   @override
   String strInput = "";
+  String name="";
+
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Form(
+      body: Form(
         key: _formKey,
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextFormField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            icon: Icon( Icons.person),
-            labelText: "Username"),
-          validator: (String? value) {
-            if (value == null || value.isEmpty) {
-              return "Please enter your name";
-            }
-          
-          },
-          onChanged: (String value) {
-            setState(() {
-              // strInput = value;
-            });
-          },
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("display : $strInput"),SizedBox(height: 20),
+            Text("display : $name"),SizedBox(height: 20),
+            TextFormField(
+              controller: _usernameController, // connect to text field
+              decoration: const InputDecoration(
+                // border: OutlineInputBorder(),
+                icon: Icon(Icons.person),
+                labelText: "Username",
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter your name";
+                }
+              },
+              onChanged: (String value) {
+                setState(() {
+                  name = value;
+                });
+              },
+
+            ),
+            TextFormField(
+              obscureText: true, //hide password
+              controller: _passwordController,
+              decoration: const InputDecoration(
+                 // connect to text field
+                icon: Icon(Icons.lock),
+                labelText: "Password",
+              ),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return "Please enter your password";
+                }
+              },
+              // onChanged: (String value) { // use checking real time
+              //   // setState(() {});
+              // },
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                if (_formKey.currentState!.validate()) {
+                  setState(() {
+                    strInput = "password: ${_passwordController.text}";
+                  });
+                } else {
+                  setState(() {
+                    strInput = "Form is invalid";
+                  });
+                }
+              },
+              child: Text("Save"),
+            ),
+          ],
         ),
-        TextFormField(
-          obscureText: true, //hide password
-          decoration: const InputDecoration(
-            icon: Icon( Icons.lock),
-            labelText: "Password"),
-          validator: (String? value) {
-            if (value == null || value.isEmpty) {
-              return "Please enter your password";
-            }
-          
-          },
-          onChanged: (String value) {
-            setState(() {
-              
-            });
-          },
-        ),SizedBox(height: 20,) ,
-        ElevatedButton(onPressed: (){
-          if(_formKey.currentState!.validate()){
-            setState(() {
-              strInput ="Form is valid";
-            });
-          }else{
-            setState(() {
-              strInput = "Form is invalid";
-            });
-          }
-        }, child:Text("Save"))
-        ],
-      ))
-    );;
+      ),
+    );
+    ;
   }
 }
